4cd3ff26aecf58061d5537c0fb5efb9a00879953
vijayprabhu04 <49646334+vijayprabhu04@users.noreply.github.com>
2022-02-04 19:54:38 +0530

    Delete all-builds/terraform directory

--- all-builds/terraform/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-100200"
-    key    = "state/terraform.tfstate"
-    region = "ap-south-1"
-    }
-}
--- all-builds/terraform/vars.tf
+++
@@ -1,4 +0,0 @@
-# Region
-variable AWS_REGION {
-  default     = "ap-south-1"
-}
