1f6ae520260f4da64bd3155be324d2f357626b8f
vijayprabhu04 <49646334+vijayprabhu04@users.noreply.github.com>
2022-02-04 19:27:29 +0530

    Update main.tf

--- all-builds/terraform/main.tf
+++ all-builds/terraform/main.tf
@@ -4,5 +4,4 @@ resource "aws_instance" "ec2_instance" {
     vpc_security_group_ids = ["sg-0fefe8e6c17c7c47c"]
     tags         = {Name = "HTTP_SERVER"}
     key_name     = "master_key"
-    count        = 1
 }
