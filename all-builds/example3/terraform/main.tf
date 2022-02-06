b497a866da24da891715fa6ad2c1d1c7f8d2b09f
vijayprabhu04 <49646334+vijayprabhu04@users.noreply.github.com>
2022-02-06 12:39:46 +0530

    Update main.tf

--- all-builds/example3/terraform/main.tf
+++ all-builds/example3/terraform/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ec2_instance" {
-    ami = "ami-052c08d70def0ac62"
+    ami = "ami-0851b76e8b1bce90b"
     instance_type = "t2.micro"
     vpc_security_group_ids = ["sg-0fefe8e6c17c7c47c"]
     tags         = {Name = "HTTP_SERVER"}
